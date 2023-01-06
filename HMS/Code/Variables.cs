7fb5d76dda6487dbe69ead387aa4dc53519ce901
anshNET <120625620+anshNET@users.noreply.github.com>
2023-01-06 13:13:11 +0530

    initialized

---
+++ HMS/Code/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Code
+{
+    public class Variables
+    {
+        public const string HotelFullName = "Hotel Vertigois";
+        public const string HotelShortName = "Vertigois";
+        public const string HotelNameAbbreviate = "HV";
+    }
+}
---
+++ HMS/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HMS.ViewModels;
+using HMS.Services;
+
+namespace HMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HomeViewModel model = new HomeViewModel();
+
+            AccomodationTypesService service = new AccomodationTypesService();
+            AccomodationPackagesService accomodationPackagesService = new AccomodationPackagesService();
+
+            model.AccomodationTypes = service.GetAllAccomodationTypes();
+            model.AccomodationPackages = accomodationPackagesService.GetAllAccomodationPackages();
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        { 
+        
+            return View();
+        }
+    }
+}
